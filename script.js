341dcb07817348a4f6db013b092abd8c4fb096c2
OlwanguKE <olwangu97@gmail.com>
2026-01-28 14:37:54 +0300

    Add to calculate area

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function calculateArea(){
+    //Collects the values from the input fields 
+    let l = document.getElementById("length").value;
+    let w = document.getElementById("width").value;
+
+    //Converts the string values to numbers 
+    let length = Number(l);
+    let width = Number(w);
+
+    //Calculates the area 
+    let area = length * width;
+
+    //Displays the result in the output field 
+    document.getElementById("result").innerHTML = "The area is  : " + area;
 
 }
